cdefe85db155e5f21c50c63f45e0547730483762
Serperior18 <issaclip@icloud.com>
2019-10-19 09:50:07 +0800

    Initial Commit

---
+++ eureka but better/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eureka but better
+//
+//  Created by student01 on 10/19/19.
+//  Copyright © 2019 student01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
