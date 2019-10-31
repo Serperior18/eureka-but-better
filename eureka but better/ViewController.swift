2f6683b55f600c2fcdcf4bdf766222298699b41b
Keith <vashtongson@gmail.com>
2019-10-31 09:58:00 +0800

    added sound

--- eureka but better/ViewController.swift
+++ eureka but better/ViewController.swift
@@ -8,17 +8,30 @@
 
 import UIKit
 import SafariServices
+import AVFoundation
 
 class ViewController: UIViewController, SFSafariViewControllerDelegate {
-   
+   var bombAudio = AVAudioPlayer()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        let sound = Bundle.main.path(forResource: "BOMB_SIREN-BOMB_SIREN-247265934", ofType: "mp3")
+        do {
+            
+            bombAudio = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+            
+        }
+        catch {
+            print(error)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBAction func soundButton(_ sender: Any) {
+        bombAudio.play()
+    }
 }
 
 
