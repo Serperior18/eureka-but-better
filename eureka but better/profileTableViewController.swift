46e84f4c0e2d81eae222beea0e285a504246dce8
Serperior18 <issaclip@icloud.com>
2019-11-06 10:52:14 +0800

    undone profile page(i blame howie)

---
+++ eureka but better/profileTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  profileTableViewController.swift
+//  eureka but better
+//
+//  Created by student01 on 11/6/19.
+//  Copyright © 2019 student01. All rights reserved.
+//
+
+import UIKit
+
+class profileTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return 0
+    }
+
+  
+}
+}
